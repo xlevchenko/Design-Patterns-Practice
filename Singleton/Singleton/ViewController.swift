a180ceb88870f69d1663052d8d37b2517e24c717
Olexsii <realcarter50@gmail.com>
2022-06-02 12:31:17 +0300

    first commit

---
+++ Singleton/Singleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton
+//
+//  Created by Olexsii Levchenko on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
